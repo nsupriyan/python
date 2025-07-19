e273453901eb6afba790b5efd634ed98fa6cdb4b
nsupriyan <nsupriyan9@gmail.com>
2025-07-19 14:19:00 +0530

    python

---
+++ tuupll.py
@@ -0,0 +1,46 @@
+# Remove duplicates from list: [5, 2,3,5,4,3,2,1] using a set
+
+# l1=[5, 2,3,5,4,3,2,1]
+# print(set(l1))
+# output:    
+# {1, 2, 3, 4, 5}
+
+# create a set of unique from the string "programming"
+# str="programming"
+# set1=set(str)
+# print(set1)
+# Output:
+# {'g', 'i', 'p', 'o', 'm', 'r', 'n', 'a'}
+
+#  Find the intersection of sets{1, 2,3} and {4, 5,6}
+
+# set1={1,2,3}
+# set2={3,4,5}
+# inter=set1 & set2
+# print(inter)
+
+# Output
+# {3}
+
+# find the Union of sets {1, 2,3} and {3, 4,5}
+
+# set1={1,2,3}
+# set2={3,4,5}
+# U=set1 | set2
+# print(U)
+
+
+
+# Output
+# {1, 2, 3, 4, 5}
+
+# Find the difference of sets { 10,20,30} and {50, 60,10,70}
+# a={10,20,30}
+# b={50, 60,10,70}
+# c=a.difference(b)
+# print(c)
+
+
+
+# Output
+# {20,30}
