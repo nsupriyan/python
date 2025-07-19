579054807e94c05a63d7a99fbc32fb4ee689dd89
nsupriyan <nsupriyan9@gmail.com>
2025-07-19 15:26:53 +0530

    python

---
+++ patternss.py
@@ -0,0 +1,13 @@
+s = "abcde"
+for i in range(len(s), 0, -1):
+    print("#", s[:i])
+
+
+s = "abcde"
+for i in range(len(s)):
+    print("#", s[i] * (len(s) - i))
+
+
+s = "abcde"[::-1]
+for i in range(len(s)):
+    print("#", s[i] * (len(s) - i))
